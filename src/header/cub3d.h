4cab55dedaecc5777fe84b23b92f7a4f488b84a5
Ricardo Gorki <ricardo.gorki@gmail.com>
2023-04-01 17:34:58 -0300

    map

--- src/header/cub3d.h
+++ src/header/cub3d.h
@@ -1,9 +1,26 @@
 #ifndef CUB3D_H
-#define CUB3D_H
+# define CUB3D_H
 
 #include "../libs/libft/libft.h"
 #include "../libs/mlx/mlx.h"
 
+/* # define NO	1
+# define SO	2
+# define EO	3
+# define WO	4
 
+# define NO ./path_to_the_north_texture
+# define SO ./path_to_the_south_texture
+# define WE ./path_to_the_west_texture
+# define EA ./path_to_the_east_texture */
+
+typedef struct	s_map
+{
+	char	**map;
+	int		has_direction;
+	int		has_textures;
+	int		has_floor;
+	int		has_ceilling;
+}				t_map;
 
 #endif
