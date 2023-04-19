ac600b7501120ea46c16fad13cbc33039000df66
RGorki <rgorki@student.42.rio>
2023-04-19 15:44:33 -0300

    need fix

--- src/validations/map/map_name.c
+++ src/validations/map/map_name.c
@@ -6,7 +6,7 @@
 /*   By: rgorki < rgorki@student.42.rio>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/19 14:44:54 by rgorki            #+#    #+#             */
-/*   Updated: 2023/04/19 14:45:09 by rgorki           ###   ########.fr       */
+/*   Updated: 2023/04/19 15:24:16 by rgorki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,12 @@ int	check_map_extension(char *map_name)
 {
 	char	*extension;
 	char	*temp_name;
-	int		result;
 
 	if (!map_name)
 		return (ret_value(1, "Missing map name: format namefile.cub"));
 	extension = ".cub";
 	temp_name = ft_strrchr(map_name, '.');
-	result = ft_strncmp(temp_name, extension, 5);
-	return (result);
+	if (!my_strncmp(temp_name, extension))
+		return (ret_value(1, "Missing map name: format namefile.cub"));
+	return (0);
 }
