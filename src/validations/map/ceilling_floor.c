e17fbe44cc411ae4eda70cd95e490c749e7775b0
RGorki <rgorki@student.42.rio>
2023-05-09 15:51:25 -0300

    up

--- src/validations/map/ceilling_floor.c
+++ src/validations/map/ceilling_floor.c
@@ -6,7 +6,7 @@
 /*   By: rgorki < rgorki@student.42.rio>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/02 16:29:20 by rgorki            #+#    #+#             */
-/*   Updated: 2023/04/25 12:45:15 by rgorki           ###   ########.fr       */
+/*   Updated: 2023/05/09 15:44:25 by rgorki           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,28 +75,59 @@ static int	check_map_floor_ceilling_utils(t_map *maps, char **split_line, int fl
 	char	**split_numbers;
 	char	**split_breakline;
 	size_t	size;
+	int  number;
+	int		i;
+	int		j;
 
+	number = 0;
+	 i = 1;
+	 size = array_counter(split_line);
+	 while(split_line[i])
+	 {
+		j = 0;
+		while (split_line[i][j] && split_line[i][j] != ',')
+		{
+			number = number + (split_line[i][j++] - '0');
+			number = number * 10;
+		}
+		if (split_line[i][j] && split_line[i][j] == ',' || i == size - 1)
+		{
+			if (split_line[0][0] == 'C')
+				if (maps->ceiling.red == -1)
+					maps->ceiling.red = number;
+				if (maps->ceiling.green == -1)
+					maps->ceiling.green = number;
+				if (maps->ceiling.blue == -1)
+					maps->ceiling.blue = number;
+			if (split_line[0][0] == 'F')
+				if (maps->floor.red == -1)
+					maps->floor.red = number;
+				if (maps->floor.green == -1)
+					maps->floor.green = number;
+				if (maps->floor.blue == -1)
+					maps->floor.blue = number;
+			number = 0;
+			printf("num: %d i: %i size: %i\n", number, i , size);
+		}
 
-	split_breakline = ft_split(split_line[1], 10);
-	split_numbers = ft_split(split_line, 44);
+		i++;
+	 }
+
+	printf("cred %d\n", maps->ceiling.red);
+	printf("cgreen %d\n", maps->ceiling.green);
+	printf("cblue %d\n\n", maps->ceiling.blue);
+	printf("fred %d\n", maps->floor.red);
+	printf("fgreen %d\n", maps->floor.green);
+	printf("fblue %d\n\n", maps->floor.blue);
 
 	size = array_counter(split_numbers);
-		printf("%s\n", split_line[2]);
+
 	if (!split_numbers)
 	{
 		free_ceilling_utils(split_breakline, split_numbers);
 		return (1);
 	}
-	if (my_strncmp(split_line[0], "F"))
-	{
-		flag = 2;
-		check_map_floor_ceilling_utils_2(maps, split_numbers, split_line[0][0]);
-	}
-	if (my_strncmp(split_line[0], "C"))
-	{
-		flag = 1;
-		check_map_floor_ceilling_utils_2(maps, split_numbers, split_line[0][0]);
-	}
+	check_map_floor_ceilling_utils_2(maps, split_numbers, split_line[0][0]);
 	free_ceilling_utils(split_breakline, split_line);
 	return (flag);
 }
